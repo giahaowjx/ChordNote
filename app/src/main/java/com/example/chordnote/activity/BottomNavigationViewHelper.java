60ab20488a2e77282071469dd19654915f2a71ef
Jiaxiao Wen <761497526@qq.com>
2019-04-07 13:03:34 +0800

    Bottom Navigation View First Commit

---
+++ app/src/main/java/com/example/chordnote/activity/BottomNavigationViewHelper.java
@@ -0,0 +1,41 @@
+package com.example.chordnote.activity;
+
+import android.annotation.SuppressLint;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+
+import java.lang.reflect.Field;
+
+public class BottomNavigationViewHelper
+{
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView view)
+    {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try
+        {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+
+            for (int i = 0; i < menuView.getChildCount(); i++)
+            {
+                BottomNavigationItemView bottomNavigationItemView = (BottomNavigationItemView)
+                        menuView.getChildAt(i);
+                bottomNavigationItemView.setShifting(false);
+                bottomNavigationItemView.setChecked(false);
+            }
+
+        }
+        catch (NoSuchFieldException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/chordnote/activity/MainActivity.java
+++ app/src/main/java/com/example/chordnote/activity/MainActivity.java
@@ -1,30 +1,12 @@
 package com.example.chordnote.activity;
 
-import android.support.annotation.NonNull;
-import android.support.design.internal.BottomNavigationPresenter;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Adapter;
-
-import com.example.chordnote.fragment.TestFragment;
-import com.example.chordnote.ui.viewpager.ViewPagerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-    private BottomNavigationActivity bottomNavigationActivity;
 
-    private ViewPagerAdapter viewPagerAdapter;
-
-    private ViewPager viewPager;
-
-    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,71 +14,9 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-    
-
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener()
-        {
-            @Override
-            public void onPageScrolled(int i, float v, int i1)
-            {
-
-            }
-
-            @Override
-            public void onPageSelected(int i)
-            {
-                if (menuItem != null)
-                {
-                    menuItem.setChecked(false);
-                }
-                else
-                {
-                    navigation.getMenu().getItem(0).setChecked(false);
-                }
-                menuItem = navigation.getMenu().getItem(i);
-                menuItem.setChecked(true);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i)
-            {
-
-            }
-        });
-
-        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(viewPagerAdapter);
-        List<Fragment> list = new ArrayList<>();
-        list.add(TestFragment.newInstance("作曲"));
-        list.add(TestFragment.newInstance("学习"));
-        list.add(TestFragment.newInstance("发现"));
-        viewPagerAdapter.setList(list);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigationView);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener()
-    {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item)
-        {
-            switch (item.getItemId())
-            {
-                case R.id.navigation_compose:
-                    viewPager.setCurrentItem(0);
-                    return true;
-                case R.id.navigation_study:
-                    viewPager.setCurrentItem(1);
-                    return true;
-                case R.id.navigation_discovery:
-                    viewPager.setCurrentItem(2);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private
 }
